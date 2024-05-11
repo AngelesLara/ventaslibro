319f7a37ef835f30b671249045aaef36be2c79a8
angeles <73196982@continental.edu.pe>
2024-05-11 09:39:51 -0500

    ACTUALIZACIÓN- ANGELES

--- app/src/main/java/com/example/ventaslibros/MainActivity.kt
+++ app/src/main/java/com/example/ventaslibros/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.ventaslibros
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val username = findViewById<View>(R.id.username) as TextView
+        val password = findViewById<View>(R.id.password) as TextView
+
+        val loginbtn = findViewById<View>(R.id.loginbtn) as MaterialButton
+
+        //admin and admin
+
+
+        //admin and admin
+        loginbtn.setOnClickListener {
+            if (username.text.toString() == "admin" && password.text.toString() == "admin") {
+                //correct
+                Toast.makeText(this@MainActivity, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+            } else  //incorrect
+                Toast.makeText(this@MainActivity, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show()
+        }
+
     }
-
-
-
-
-
 }
